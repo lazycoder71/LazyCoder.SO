1ce4cab500856c9570299b797ca56da0611e5c35
lazycoder71 <ndlam1994@gmail.com>
2025-07-09 17:24:25 +0700

    + Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Installation
+
+1. Open the Package Manager from Window > Package Manager.
+2. Click the "+" button > Add package from git URL.
+3. Enter the following URL:
+
+```
+https://github.com/lazycoder71/LazyCoder.SO.git
+```
---
+++ Runtime/ScriptableObjectSingleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace LazyCoder.SO
+{
+    public abstract class ScriptableObjectSingleton<T> : ScriptableObject where T : ScriptableObject
+    {
+        private static readonly string s_rootFolderName = "ScriptableObjectSingleton";
+
+        private static T s_instance = null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    s_instance = Resources.Load<T>($"{s_rootFolderName}/{typeof(T)}");
+
+#if UNITY_EDITOR
+                    if (s_instance == null)
+                    {
+                        string configPath = $"Assets/_ROOT/Resources/{s_rootFolderName}/";
+
+                        if (!System.IO.Directory.Exists(configPath))
+                            System.IO.Directory.CreateDirectory(configPath);
+
+                        s_instance = ScriptableObjectHelper.CreateAsset<T>(configPath, typeof(T).ToString());
+                    }
+#endif
+                }
+
+                return s_instance;
+            }
+        }
+    }
+}
